4417dae8e4693fc8dc25e498effd3e0e57f0f54f
Nhlanhla CliQ <nhlanhlacliq@gmail.com>
2024-10-10 01:59:53 +0200

    update table schemas

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -2,27 +2,57 @@ import {
   boolean,
   integer,
   pgTable,
+  serial,
   text,
   timestamp,
   varchar,
 } from "drizzle-orm/pg-core";
+import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 
 export const usersTable = pgTable("users", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  id: serial("id").primaryKey(),
   email: varchar({ length: 255 }).notNull().unique(),
   username: varchar({ length: 255 }).notNull().unique(),
   passwordHash: varchar({ length: 255 }).notNull(),
-  createdAt: timestamp("created_at").defaultNow(),
-  updatedAt: timestamp("updated_at").$onUpdate(() => new Date()),
+  createdAt: timestamp("created_at", { withTimezone: true })
+    .defaultNow()
+    .notNull(),
+  updatedAt: timestamp("updated_at", { withTimezone: true })
+    .defaultNow()
+    .$onUpdate(() => new Date()),
+});
+
+export const selectUsersSchema = createSelectSchema(usersTable);
+export const insertUsersSchema = createInsertSchema(usersTable, {
+  email: (schema) => schema.email.email(),
+  username: (schema) => schema.username.min(1),
+}).omit({
+  id: true,
+  createdAt: true,
+  updatedAt: true,
 });
 
 export const inventoryTable = pgTable("inventory", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  id: serial("id").primaryKey(),
   name: text().notNull(),
   count: integer().notNull(),
   isAvailable: boolean().notNull(),
-  createdAt: timestamp("created_at").defaultNow(),
-  updatedAt: timestamp("updated_at").$onUpdate(() => new Date()),
-  updatedBy: integer().references(() => usersTable.id),
+  createdAt: timestamp("created_at", { withTimezone: true })
+    .defaultNow()
+    .notNull(),
+  updatedAt: timestamp("updated_at", { withTimezone: true })
+    .defaultNow()
+    .$onUpdate(() => new Date()),
   //   imageUrl: varchar({ length: 255 }),
+});
+
+export const selectInventorySchema = createSelectSchema(inventoryTable);
+
+export const insertInventorySchema = createInsertSchema(inventoryTable, {
+  name: (schema) => schema.name.min(1),
+  count: (schema) => schema.count.min(0),
+}).omit({
+  id: true,
+  createdAt: true,
+  updatedAt: true,
 });
