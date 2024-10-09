9a2275895a54c8be3eb942c67b4cb48fd704df02
nhlanhlacliq <nhlanhlacliq@gmail.com>
2024-10-09 10:40:12 +0200

    refactor app creation

---
+++ src/lib/create-base-app.ts
@@ -0,0 +1,27 @@
+import { OpenAPIHono } from "@hono/zod-openapi";
+import { cors } from "hono/cors";
+import { logger } from "hono/logger";
+
+import notFound from "@/middleware/not-found";
+import onError from "@/middleware/on-error";
+import serveEmojiFavicon from "@/middleware/serve-emoji-favicon";
+
+// Structed like this so we can attach documented routes (without middleware added by create base app)
+export function createStandaloneApp() {
+  // OpenApiHono is just a wrapper around Hono (https://hono.dev/examples/zod-openapi) enables easier api documentation(openApi)
+  return new OpenAPIHono({ strict: false });
+}
+
+export default function createBaseApp() {
+  const app = createStandaloneApp();
+  // Middlewares
+  app.use(logger());
+  app.use(serveEmojiFavicon("🍞")); // Serves an emoji string when browsers request /favicon.ico. Also the favicon on the browser tab
+
+  app.use(cors());
+
+  app.notFound(notFound);
+  app.onError(onError);
+
+  return app;
+}
