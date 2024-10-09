e00ebef9ed20e196274029af88710b063581b4a6
nhlanhlacliq <nhlanhlacliq@gmail.com>
2024-10-09 19:58:06 +0200

    add database url to env

--- env.ts
+++ env.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 export const envSchema = z.object({
   NODE_ENV: z.enum(["development", "production"]).default("development"),
   PORT: z.coerce.number().default(8080),
+  DATABASE_URL: z.string().url(),
 });
 
 export const env = envSchema.parse(process.env);
