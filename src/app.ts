3fb8ea8ff1d4a1fc9332899a81d135e579bfeaf9
Nhlanhla CliQ <nhlanhlacliq@gmail.com>
2024-10-10 08:57:33 +0200

    add auth routes to app

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import createOpenApiDoc from "./lib/create-open-api-doc";
 import error from "@/routes/error.route";
 import index from "@/routes/index.route";
 import inventory from "@/routes/inventory/inventory.route";
+import auth from "@/routes/auth/auth.route";
 
 const app = createBaseApp();
 
@@ -11,7 +12,7 @@ const app = createBaseApp();
 createOpenApiDoc(app);
 
 // Routes
-const routes = [index, error, inventory];
+const routes = [auth, inventory, index, error];
 
 routes.forEach((route) => {
   app.route("/", route);
