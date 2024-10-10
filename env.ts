fa60493e7479b94efd9885fe956bfd056ef3eb88
Nhlanhla CliQ <nhlanhlacliq@gmail.com>
2024-10-10 08:57:11 +0200

    update env

--- env.ts
+++ env.ts
@@ -5,6 +5,7 @@ export const envSchema = z.object({
   NODE_ENV: z.enum(["development", "production"]).default("development"),
   PORT: z.coerce.number().default(8080),
   DATABASE_URL: z.string().url(),
+  JWT_SECRET: z.string(),
 });
 
 export const env = envSchema.parse(process.env);
