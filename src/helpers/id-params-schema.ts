f04cac7f8ec5b0074603be1cb2eda120ce820ad5
Nhlanhla CliQ <nhlanhlacliq@gmail.com>
2024-10-10 02:00:04 +0200

    create helper

---
+++ src/helpers/id-params-schema.ts
@@ -0,0 +1,14 @@
+import { z } from "@hono/zod-openapi";
+
+const IdParamsSchema = z.object({
+  id: z.coerce.number().openapi({
+    param: {
+      name: "id",
+      in: "path",
+    },
+    required: ["id"],
+    example: 42,
+  }),
+});
+
+export default IdParamsSchema;
